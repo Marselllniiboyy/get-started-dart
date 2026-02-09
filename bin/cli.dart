55b9cd827369a6937fe188d91b74fd195a8c03f1
Marselllniiboyy <marseljayawijaya@gmail.com>
2026-02-09 12:47:24 +0800

    Chapter 3 done: Build asyncronus code wit wikipedia API

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 // Implement version and help commands
 const version = '0.0.1';
 void main(List<String> arguments) {
@@ -6,7 +7,7 @@ void main(List<String> arguments) {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
-  }else if (arguments.first == 'search') {
+  }else if (arguments.first == 'wikipedia') {
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
     searchWikipedia(inputArgs);
   } else {
@@ -14,22 +15,43 @@ void main(List<String> arguments) {
   }
 }
 
-void searchWikipedia(List<String>? arguments) {
+void searchWikipedia(List<String>? arguments) async {
   final String articleTitle;
 
   if (arguments == null || arguments.isEmpty) {
-    print('Please provide and article title to search for.');
-    articleTitle = stdin.readLineSync() ?? '';
+    print('Please provide an article title.');
+    final inputFromStdin = stdin.readLineSync(); // Read input
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print('No article title provided. Exiting.');
+      return; // Exit the function if no valid input
+    }
+    articleTitle = inputFromStdin;
   } else {
     articleTitle = arguments.join(' ');
   }
+
   print('Looking up articles about "$articleTitle". Please wait.');
-  print('Here ya go!');
-  print('(Pretend this is an article about "$articleTitle")');
+  
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent);
 }
 
 void printUsage() {
   print(
     "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
   );
+}
+
+Future<String> getWikipediaArticle(String articleTitle) async {
+  final url = Uri.https(
+    'en.wikipedia.org', //Wikipedia API base URL
+    '/api/rest_v1/page/summary/$articleTitle', //Endpoint for summary
+  );
+
+  final response = await http.get(url);
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+
+  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
 }
